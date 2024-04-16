40db34e8c38fa0821e1fd1f16b24e2351b0613d6
Ryan Gaus <rsg1egoman@gmail.com>
2024-04-15 21:24:35 -0400

    feat: add controls

---
+++ example/src/admin/controls/index.tsx
@@ -0,0 +1,43 @@
+import { useContext, useMemo } from 'react';
+import { AdminContext } from '..';
+
+import AppBar from './AppBar';
+import Button, { IconButton, NavigationButton } from './Button';
+import Checkbox from './Checkbox';
+import Modal from './Modal';
+import Popover from './Popover';
+import Radiobutton from './Radiobutton';
+import Select from './Select';
+import TextArea from './TextArea';
+import TextInput from './TextInput';
+
+// These control implementations are what the admin panel uses by default
+// They can be easily overriden by setting them on `AdminContext`
+const baseControls = {
+  AppBar,
+  Button,
+  IconButton,
+  NavigationButton,
+  Checkbox,
+  Modal,
+  Popover,
+  Radiobutton,
+  Select,
+  TextArea,
+  TextInput,
+};
+
+export type Controls = Partial<typeof baseControls>;
+
+
+export const useControls = () => {
+  const adminContext = useContext(AdminContext);
+
+  return useMemo(() => {
+    if (!adminContext || !adminContext.controls) {
+      return baseControls;
+    }
+
+    return { ...baseControls, ...adminContext.controls };
+  }, [adminContext]);
+};
