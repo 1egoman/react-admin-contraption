ac6df0a17f91e9a0ad7bca48ddb7dc3203ded385
Ryan Gaus <rsg1egoman@gmail.com>
2024-05-31 11:57:05 -0400

    fix: try to adjust build process to make package work for typescript
    Typescript seems to be unable to read the `exports` key in package.json,
    so copy all the files into the root instead.

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ npx tsup \
   --external react-dom \
   --external next \
   admin/index.tsx admin/global.css admin/**/*.ts admin/**/*.tsx
+
+# After building, copy everything in dist into the package root
+# This is because typescript doesn't seem to support reading the `exports` package.json key.
+cp -R dist/* .
