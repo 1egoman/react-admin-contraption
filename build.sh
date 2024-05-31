812b04823cbfc4e08c7f78e0e0e92ed2923711fe
Ryan Gaus <rsg1egoman@gmail.com>
2024-05-31 12:16:55 -0400

    feat: publish non-minified code instead
    This is to debug a peer dependency issue

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 npx tsup \
   --clean \
-  --minify \
   --dts \
   --external react \
   --external react-dom \
   --external next \
   admin/index.tsx admin/global.css admin/**/*.ts admin/**/*.tsx
+  # --minify \
 
 # After building, copy everything in dist into the package root
 # This is because typescript doesn't seem to support reading the `exports` package.json key.
